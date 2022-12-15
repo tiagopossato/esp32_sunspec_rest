3f048920c175fa139b9504e010d2d1b652a86134
Tiago Possato <tiago.possato@yahoo.com.br>
2022-12-15 15:29:18 -0300

    comentários no monitor

--- suns_monitor/app/save_data.py
+++ suns_monitor/app/save_data.py
@@ -1,5 +1,5 @@
 """
-S save messages to the database.
+Save messages to the database.
 """
 import os
 import sys
@@ -11,22 +11,40 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, JSON, DateTime, String
 
+# path to the database
 DB_PATH = "/opt/suns_monitor/db/suns_monitor.db"
 
+# create the database
 Base = declarative_base()
 class Data(Base):
+    """
+    Class to represent the data table.
+    """
+    # nome da tabela
     __tablename__ = 'data'
+    # colunas
+    # Identificador único de cada linha
     id = Column(Integer, primary_key=True)
+    # Modelo SunSpec
     model =  Column(Integer)
+    # Instância do modelo
     instance =  Column(Integer)
+    # Ponto do modelo
     point =  Column(String)
+    # Valor do ponto. 
+    # Armazenado como JSON para aceitar diferentes tipos de dados
+    # Manter o seguinte padrão: {"value": <valor>}
     value = Column(JSON)
+    # Data e hora da leitura
     timestamp = Column(Integer)  
 
     def __repr__(self):
         return f'{{"model":{self.model}, "instance":{self.instance}, "point":{self.point}, "value":{self.value}, "timestamp":{datetime.fromtimestamp(self.timestamp)}}}'
 
 def start_db_session():
+    """
+    Start a session with the database.
+    """
     print(f"DB_PATH: {DB_PATH}")
     engine = create_engine(f'sqlite:///{DB_PATH}', echo=False)
     Session = sessionmaker(bind=engine)
@@ -35,13 +53,19 @@ def start_db_session():
     return session
 
 def save_data(model, instance, point, value, timestamp, session):
+    """
+    Save data to the database.
+    """
     try:
+        # create the data object
         data = Data(model = model,
             instance = instance,
             point = point,
             value = {"value": value},
             timestamp = timestamp)
+        # save the data
         session.add(data)
+        # commit the data
         session.commit()
         print(data)
     except Exception as e:
--- suns_monitor/app/suns_monitor.py
+++ suns_monitor/app/suns_monitor.py
@@ -1,3 +1,6 @@
+"""
+A simple example of how to get data from a SunSpec device with REST API.
+"""
 import requests
 from requests.auth import HTTPBasicAuth
 from threading import Thread
@@ -8,34 +11,51 @@ from datetime import datetime
 requests.packages.urllib3.disable_warnings()
 print("\nATENÇÃO, WARNINGS DESABILITADOS\n")
 
-server_url = "https://192.168.4.1/v1/"
+# variables to connect to the REST API
+server_url = "https://192.168.1.27/v1/"
+# parameters to get data
+model = 307
+instance = 0
+points = "TmpAmb,RH,Pres"
 
+# variables to calculate average request time
 avg=0
 count=0
+
+# interval between requests
 interval=5
 
 if __name__ == '__main__':
+    # start the database session
     session = start_db_session()
-
+    # loop forever
     while(True):
+        # get the initial datetime
         init_time = datetime.now()
         print(f"Requesting values at: {init_time}")
         try:
-            r = requests.get(f"{server_url}models/307/instances/0?points=TmpAmb,RH,Pres", verify=False, timeout=5, auth=HTTPBasicAuth("admin", "admin"))
+            # get the data over REST API
+            r = requests.get(f"{server_url}models/{model}/instances/{instance}?points={points}", verify=False, timeout=5, auth=HTTPBasicAuth("admin", "admin"))
+            # get the final datetime
             end_time = datetime.now()
+            # calculate the request time
             request_time=(datetime.now() - init_time).total_seconds()
+            # calculate the average request time
             count = count + 1
             avg = ((avg*(count-1))+request_time)/count
             print(f"Request time: {request_time:.4}s, avg: {avg:.4}s, count: {count}")
-            print(r.status_code)
 
-            resposta = r.json()['models'][0]
+            # print the response code
+            print(r.status_code)
+            # get the json response
+            data_points = r.json()['models'][0]
             
             # iterate over json keys and save data
-            for key in resposta:
-                # ignore name and id
+            for key in data_points:
+                # ignore 'name' and 'id' keys
                 if key != 'name' and key != 'id':
-                    save_data(model=307, instance=0, point=key, value=resposta[key], timestamp=time(), session=session)
+                    save_data(model=model, instance=instance, point=key, value=data_points[key], timestamp=time(), session=session)
+            # sleep until the next request
             try:
                 sleep(interval-(end_time - init_time).total_seconds())
             except:
